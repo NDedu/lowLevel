1282b5325dcf947a4b9175655474c52aba72b0cb
Dedu <94352370+NDedu@users.noreply.github.com>
2026-05-27 12:03:20 +0300

    Update kv.h

--- inc/kv.h
+++ inc/kv.h
@@ -17,6 +17,7 @@ typedef struct {
 kv_t *kv_init(size_t capacity); // returns a kv_t pointer
 int kv_put(kv_t *db, char *key, char *value); // returns the index, -1 on error, -2 if full
 char *kv_get(kv_t *db, char *key);
+int kv_delete(kv_t *db, char *key);
 void kv_free(kv_t *table);
 
 #endif
