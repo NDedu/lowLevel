bc73c283fbfce50057bb856f29173cc4d4b43055
Dedu <94352370+NDedu@users.noreply.github.com>
2026-05-27 12:29:49 +0300

    Update kv.h

--- inc/kv.h
+++ inc/kv.h
@@ -2,6 +2,7 @@
 #define KV_H
 
 #include <stdlib.h>
+#include <sys/types.h>
 
 typedef struct {
     char    *key;
@@ -15,9 +16,9 @@ typedef struct {
 } kv_t;
 
 kv_t *kv_init(size_t capacity); // returns a kv_t pointer
-int kv_put(kv_t *db, char *key, char *value); // returns the index, -1 on error, -2 if full
-char *kv_get(kv_t *db, char *key);
-int kv_delete(kv_t *db, char *key);
-void kv_free(kv_t *table);
+ssize_t kv_put(kv_t *db, char *key, char *value); // returns the index, -1 on error, -2 if full
+char *kv_get(kv_t *db, char *key); // returns the pointer to the value, NULL if not found
+ssize_t kv_delete(kv_t *db, char *key); // returns the index, -1 on not found
+int kv_free(kv_t *table); // returns 0 on success or -1 on failure, user must NULL db on free
 
 #endif
